380e6024db551a6c1f434a0985a28462dbe4db36
Tcx7 <98015500+Tcx7@users.noreply.github.com>
2022-03-01 15:45:24 -0500

    async function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,7 +2,7 @@ var userInput = document.getElementById("userInput");
 var searchBtn = document.getElementById("searchBtn");
 var weatherContent = document.getElementById("weather-content");
 
-searchBtn.addEventListener("click", weather_function);
+searchBtn.addEventListener("click", findWeather);
 
 function weather_function(e) {
   weatherContent.innerHTML = "";
@@ -28,6 +28,24 @@ function weather_function(e) {
     });
 }
 
-async function findWeather() {
-  console.log("ee");
+async function findWeather(e) {
+  weatherContent.innerHTML = "";
+  e.preventDefault();
+  let res = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=b97038a6315e4c7cb4725000220802&q=${userInput.value}&aqi=no`
+  );
+  let content = await res.json();
+  console.log(content);
+  let createElem = document.createElement("h1");
+  let createLocation = document.createTextNode(content.location.name);
+  createElem.appendChild(createLocation);
+  createElem.classList.add("stats");
+  weatherContent.appendChild(createElem);
+  let createElem2 = document.createElement("h1");
+  let createFaren = document.createTextNode(
+    ` is ${content.current.temp_f}° right now.`
+  );
+  createElem2.appendChild(createFaren);
+  createElem2.classList.add("stats");
+  weatherContent.appendChild(createElem2);
 }
